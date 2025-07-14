007032685053def09a6031706ba12981ccaa3d91
Mialy-Steeven <gonkirua586@gmail.com>
2025-07-14 09:02:19 +0300

    inc

---
+++ inc/connexion.php
@@ -0,0 +1,15 @@
+<?php
+function dbconnect()
+{
+    static $connect = null;
+    if ($connect === null) {
+        //$connect = mysqli_connect('172.60.255.248', 'ETU004112', 'PxVufPVA', 'db_s2_ETU004112');
+        $connect = mysqli_connect('localhost', 'root', '', 'employees');
+        if (!$connect) {
+            die('Erreur de connexion à la base de données : ' . mysqli_connect_error());
+        }
+        mysqli_set_charset($connect, 'utf8mb4');
+    }
+    return $connect;
+}
+?>
