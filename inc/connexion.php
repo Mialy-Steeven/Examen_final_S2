138e0f7ffa7a4b79761c43e5f55d5cbf45e06c15
Mialy-Steeven <gonkirua586@gmail.com>
2025-07-14 13:06:20 +0300

    inc

--- inc/connexion.php
+++ inc/connexion.php
@@ -3,7 +3,7 @@ function dbconnect()
 {
     static $connect = null;
     if ($connect === null) {
-        //$connect = mysqli_connect('172.60.255.248', 'ETU004112', 'PxVufPVA', 'db_s2_ETU004112');
+       // $connect = mysqli_connect('172.60.0.15', 'ETU004112', 'PxVufPVA', 'db_s2_ETU004112');
         $connect = mysqli_connect('localhost', 'root', '', 'partage_objets');
         if (!$connect) {
             die('Erreur de connexion à la base de données : ' . mysqli_connect_error());
