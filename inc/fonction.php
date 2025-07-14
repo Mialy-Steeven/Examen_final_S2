30eccc358202a92d6390479feacdd1c0a1bcb856
T5siory <t.niryavo@gmail.com>
2025-07-14 14:56:39 +0300

    inc

--- inc/fonction.php
+++ inc/fonction.php
@@ -241,5 +241,92 @@ function recherche_jiaby($recherche)
     return $res;
 }
 
+function recherche_par_categorie($categorie) {
+    $conn = dbconnect();
+    $categorie = mysqli_real_escape_string($conn, $categorie);
+
+    $sql = "SELECT o.nom_objet, co.nom_categorie, o.disponible
+            FROM objet o
+            JOIN categorie_objet co ON co.id_categorie = o.id_categorie
+            WHERE co.nom_categorie = '$categorie'";
+
+    $req = mysqli_query($conn, $sql);
+
+    $res = array();
+    while ($row = mysqli_fetch_assoc($req)) {
+        $res[] = $row;
+    }
+    return $res;
+}
+
+function recherche_par_nom($nom) {
+    $conn = dbconnect();
+    $nom = mysqli_real_escape_string($conn, $nom);
+
+    $sql = "SELECT o.nom_objet, co.nom_categorie, o.disponible
+            FROM objet o
+            JOIN categorie_objet co ON co.id_categorie = o.id_categorie
+            WHERE o.nom_objet LIKE '%$nom%'";
+
+    $req = mysqli_query($conn, $sql);
+
+    $res = array();
+    while ($row = mysqli_fetch_assoc($req)) {
+        $res[] = $row;
+    }
+    return $res;
+}
+
+function recherche_combinee($categorie, $nom, $disponible) {
+    $conn = dbconnect();
+
+    $conditions = array();
+
+    if ($categorie != "") {
+        $categorie = mysqli_real_escape_string($conn, $categorie);
+        $conditions[] = "co.nom_categorie = '$categorie'";
+    }
+
+    if ($nom != "") {
+        $nom = mysqli_real_escape_string($conn, $nom);
+        $conditions[] = "o.nom_objet LIKE '%$nom%'";
+    }
+
+    $where = "";
+    if (!empty($conditions)) {
+        $where = "WHERE " . implode(" AND ", $conditions);
+    }
+
+    $sql = " SELECT 
+            o.id_objet,
+            o.nom_objet,
+            co.nom_categorie,
+            IF(e.id_objet IS NULL, 1, 0) AS disponible
+        FROM objet o
+        JOIN categorie_objet co ON co.id_categorie = o.id_categorie
+        LEFT JOIN (
+            SELECT DISTINCT id_objet
+            FROM emprunt
+            WHERE date_retour IS NULL
+        ) e ON o.id_objet = e.id_objet
+        $where
+    ";
+
+    if ($disponible) {
+        $sql = "SELECT * FROM ($sql) as tmp WHERE tmp.disponible = 1";
+    }
+
+    $req = mysqli_query($conn, $sql);
+
+    $res = array();
+    while ($row = mysqli_fetch_assoc($req)) {
+        $res[] = $row;
+    }
+    return $res;
+}
+
+
+
+
 
 ?>
